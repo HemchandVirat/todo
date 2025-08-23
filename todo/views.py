99a974d2ffb8f47cbdf800f517c5dc98cabe7544
Hemchand Cracker <crackerace04@gmail.com>
2025-08-23 15:24:18 +0530

    Delete Task - functionality

--- todo/views.py
+++ todo/views.py
@@ -21,6 +21,7 @@ def mark_as_undone(request, pk):
     task.save()
     return redirect('home')
 
+
 def edit_task(request, pk):
     get_task = get_object_or_404(Task, pk=pk)
     if request.method == "POST":
@@ -33,3 +34,8 @@ def edit_task(request, pk):
             'get_task': get_task,
         }
         return render(request, 'edit_task.html', context)
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
