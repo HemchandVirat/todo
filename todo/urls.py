3aa06c49a489c710707f2aef8f38debd4f25ee39
Hemchand Cracker <crackerace04@gmail.com>
2025-08-23 11:32:28 +0530

    Mark as done,Undone & edit task

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add Task
     path('addTask/', views.addTask, name='addTask'),
+    # Mark as Done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    # Mark as Undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # edit Task
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
 ]
     
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, get_object_or_404,render
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,5 +7,21 @@ from .models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
-
     return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+
+    
+    return render(request, 'edit_task.html')
